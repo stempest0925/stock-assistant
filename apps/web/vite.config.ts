3f40e2e7aa4065319e0366db26b14e00406beea2
lucas <stempest@163.com>
2025-11-05 18:45:09 +0800

    添加 tailwindcss 支持。

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,12 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
