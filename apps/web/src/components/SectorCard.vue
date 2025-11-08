4a39385a6ff75caa63cfc3d698c020daf268284a
lucas <stempest@163.com>
2025-11-08 17:32:51 +0800

    添加动画库，做卡片入场动画以及悬浮视差。

--- apps/web/src/components/SectorCard.vue
+++ apps/web/src/components/SectorCard.vue
@@ -1,22 +1,60 @@
 <script setup lang="ts">
+import gsap from "gsap";
+
 import BaseIcon from "./BaseIcon.vue";
 import AreaChart from "./AreaChart.vue";
+
+const onMouseEnter = (event: MouseEvent) => {
+  const target = event.currentTarget as HTMLElement;
+  const parent = target.parentElement;
+  const targetIndex = Number(target.dataset.index);
+
+  if (parent) {
+    const cards = parent.querySelectorAll(".sector-card");
+
+    cards.forEach((card) => {
+      if (card === target) {
+        gsap.to(card, { y: 0, scale: 1.05, duration: 0.6, ease: "expo.out" });
+      } else {
+        const cardIndex = Number((card as HTMLElement).dataset.index);
+        const cardY = (cardIndex - targetIndex) * -16; // 通过运算得出Y距离，形成视差
+
+        gsap.to(card, { y: cardY, scale: 0.98, duration: 0.6, ease: "expo.out" });
+      }
+    });
+  }
+};
+const onMouseLeave = (event: MouseEvent) => {
+  const target = event.currentTarget as HTMLElement;
+  const parent = target.parentElement;
+
+  if (parent) {
+    const cards = parent.querySelectorAll(".sector-card");
+    cards.forEach((card) => {
+      gsap.to(card, { y: 0, scale: 1, duration: 0.6, ease: "expo.out" });
+    });
+  }
+};
 </script>
 
 <template>
-  <div class="flex flex-row items-center p-2 bg-white rounded-xl shadow-md">
-    <div class="px-6 text-2xl font-bold">01</div>
+  <div
+    class="sector-card flex flex-row items-center p-2 bg-white rounded-2xl"
+    @mouseenter="onMouseEnter"
+    @mouseleave="onMouseLeave"
+  >
+    <i class="px-6 text-2xl font-black text-shadow-lg">01</i>
     <div class="flex-1 flex flex-col px-6 py-2 space-y-2 border-l border-gray-400">
       <div class="flex flex-row space-x-4 items-baseline">
-        <div class="w-8 aspect-square">
+        <!-- <div class="w-8 aspect-square">
           <BaseIcon name="cn" />
-        </div>
-        <h3 class="text-2xl font-medium">汽车零部件</h3>
-        <span class="bg-gray-200/80 px-2 rounded">912044</span>
+        </div> -->
+        <h3 class="text-2xl font-bold tracking-wider text-shadow-lg">汽车零部件</h3>
+        <span class="bg-[#f7f9fc] px-2 rounded">912044</span>
       </div>
       <div class="flex flex-row items-center space-x-4">
-        <span class="px-4 py-0.5 bg-green-200 rounded-xl">申万二级</span>
-        <div class="flex flex-row bg-gray-200/80 rounded-xl px-4 py-0.5 items-center">
+        <span class="px-4 py-0.5 bg-[#f7f9fc] rounded-xl">申万二级</span>
+        <div class="flex flex-row bg-[#f7f9fc] rounded-xl px-4 py-0.5 items-center">
           <div class="w-8 aspect-square">
             <BaseIcon name="cn" />
           </div>
@@ -24,7 +62,7 @@ import AreaChart from "./AreaChart.vue";
         </div>
       </div>
     </div>
-    <div class="w-[280px] h-[160px] overflow-hidden rounded-lg">
+    <div class="w-[280px] h-[140px] overflow-hidden rounded-lg">
       <AreaChart />
     </div>
   </div>
--- apps/web/src/views/CNStockView.vue
+++ apps/web/src/views/CNStockView.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
-// import TheWelcome from '../components/TheWelcome.vue'
+import gsap from "gsap";
+
 import SectorCard from "@/components/SectorCard.vue";
+
+const onBeforeEnter = (el: Element) => {
+  const _el = el as HTMLElement; // 解决Element基类无法访问style
+  _el.style.opacity = "0";
+  _el.style.transform = "translateY(30px)";
+};
+const onEnter = (el: Element, done: () => void) => {
+  const _el = el as HTMLElement; // 解决Element基类无法访问dataset
+  const delay = Number(_el.dataset.index) * 0.3;
+  gsap.to(el, { opacity: 1, translateY: 0, delay, duration: 0.6, onComplete: done });
+};
 </script>
 
 <template>
   <main class="container mx-auto pt-10">
     <!-- <img src="@/assets/images/banner.png" /> -->
-    <SectorCard />
+    <TransitionGroup
+      tag="div"
+      class="space-y-6"
+      :css="false"
+      appear
+      @before-enter="onBeforeEnter"
+      @enter="onEnter"
+    >
+      <SectorCard v-for="sector in 10" :key="sector" :data-index="sector" />
+    </TransitionGroup>
   </main>
 </template>
+<style lang="css" scoped></style>
